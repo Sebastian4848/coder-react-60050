6d60a56a774e732e2e6beb454956bb69d2932c52
Sebastian Ojeda <sebastian.ojeda.msc@gmail.com>
2024-09-24 22:52:13 -0300

    2 diferentes estados funcionanco

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Greeting = (props) => {
-    console.log(props);
 
-  return (
-    <>
-    <h1>{props.message}</h1>
-    <button>Hola!</button>
-    </>
-  )
+    const [count, setCount] = useState(0)
+    const [countHover, setCountHover] = useState(0)
+
+    const handleClick = () => {
+        setCount(prevCount => prevCount + 1)
+    }
+const handleHover = () => {
+    setCountHover(prevCountHover => prevCountHover + 1)
 }
 
-export default Greeting
+
+    return (
+        <>
+            <h1>{props.message}</h1>
+            <button onClick={handleClick} >Hola!</button>
+            <span>{count}</span>
+            <div style={{width: 300, height: 200, backgroundColor: "gray"}} onMouseEnter={handleHover}>
+                <span>{countHover}</span>
+            </div>
+        </>
+    )
+}
+
+export default Greeting;
