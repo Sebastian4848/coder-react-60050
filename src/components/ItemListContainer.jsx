758022b85b6277c97a55dff4474074d76c08d438
Sebastian Ojeda <sebastian.ojeda.msc@gmail.com>
2024-09-24 23:54:24 -0300

    limpiado y ordenado de los archivos

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const ItemListContainer = (greeting) => {
-  return (
-    <div>{greeting.message}</div>
-  )
+    return (
+        <div>{greeting.message}</div>
+    )
 }
 
 export default ItemListContainer
