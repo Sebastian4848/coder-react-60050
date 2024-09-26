76ebd439d4cda66c4acc3c7f29bb5d4d82e3d9c3
Sebastian Ojeda <sebastian.ojeda.msc@gmail.com>
2024-09-25 23:53:17 -0300

    arreglo de los estilos del navbar

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -4,8 +4,10 @@ import cart from '../assets/cart.svg'
 const CartWidget = () => {
     return (
         <>
+        <div>
         <img src={cart} alt='cart' style={{ width: 40 }} />
         <span>(3)</span>
+        </div>
         </>
     )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,12 +6,20 @@ import CartWidget from './CartWidget'
 const NavBar = () => {
     return (
         <ul>
-            <li><a className="active" href="#home">Home</a></li>
-            <li><a href="#news">News</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="#about">About</a></li>
-            <h1>Tienda WauDogShop</h1>
-            <CartWidget />
+            <div>
+                <li><a className="active" href="#home">Home</a></li>
+                <li><a href="#news">News</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#about">About</a></li>
+            </div>
+
+            <div>
+                <h1>Tienda WauDogShop</h1>
+            </div>
+            
+            <div>
+                <CartWidget />
+            </div>
         </ul>
     )
 }
